96d2f3fe44cd7ac9ca32512970b021396bdd06ce
YONATAN TZULANG <YONATZ@rafael.co.il>
2024-06-16 16:53:37 +0300

    first commit

---
+++ compute_example.glsl
@@ -0,0 +1,24 @@
+#[compute]
+#version 450
+
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+layout(r8, binding = 0) uniform image2D output_data;
+layout(r8, binding = 1) uniform image2D input_data;
+	
+ 
+
+void main() {
+	ivec2 uv = ivec2(gl_GlobalInvocationID.xy);
+
+ 	ivec2 uv_input = uv * 2;
+
+
+ 	float value = 		   	  imageLoad(input_data , uv_input		 		).x;
+ 	  	  value = max( value, imageLoad(input_data , uv_input + ivec2(1, 0)	).x);
+ 	  	  value = max( value, imageLoad(input_data , uv_input + ivec2(1, 1)	).x);
+ 	  	  value = max( value, imageLoad(input_data , uv_input + ivec2(0, 1)	).x);
+ 	  
+	vec4 pixel = vec4(vec3(value), 0.0);
+	imageStore(output_data, uv, pixel);
+
+}
---
+++ first_scene.gd
@@ -0,0 +1,113 @@
+extends Sprite2D
+ 
+
+
+ 
+
+var _bind_counter: int = 0;
+
+func _init_heightmap():
+	
+	#var noise := FastNoiseLite.new()
+	#noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	#noise.fractal_octaves = 5
+	#noise.fractal_type = FastNoiseLite.FRACTAL_FBM
+	#noise.fractal_lacunarity = 2
+	#noise.fractal_gain = 2
+	#noise.seed = 0
+	#noise.frequency = 0.0194
+	#var heightmap2 := noise.get_image(512, 512, false, false)
+	var heightmap := Image.new()
+	heightmap.copy_from( texture.get_image())
+	heightmap.convert(Image.FORMAT_L8)
+	#print(heightmap2.get_format() == heightmap.get_format())
+	#print(heightmap.get_size())
+	#print(heightmap.data['data'].size())
+	#print(heightmap2.data['data'].size())
+	return heightmap
+	
+
+func _init_output_texture(rd: RenderingDevice, factor = 1):
+	var fmt := RDTextureFormat.new()
+	
+	@warning_ignore("integer_division")
+	fmt.width = texture.get_width() / factor
+	
+	@warning_ignore("integer_division")
+	fmt.height = texture.get_height()/ factor
+	fmt.format = RenderingDevice.DATA_FORMAT_R8_UNORM
+	fmt.usage_bits = RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT | \
+					 RenderingDevice.TEXTURE_USAGE_STORAGE_BIT | \
+					 RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT
+
+	#var image := Image.create(texture.get_width(), texture.get_height(), false, Image.FORMAT_L8)
+	#var data_texture : ImageTexture	 = ImageTexture.create_from_image(image)	
+	var output_data_texture: RID = rd.texture_create(fmt, RDTextureView.new())
+	
+	var output_data_uniform := RDUniform.new()
+	output_data_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
+	output_data_uniform.binding = _bind_counter
+	output_data_uniform.add_id(output_data_texture)
+	
+	_bind_counter+=1
+	
+	return {'texture': output_data_texture, 'uniform': output_data_uniform}
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	await texture.changed
+	# We will be using our own RenderingDevice to handle the compute commands
+	var rd : RenderingDevice = RenderingServer.create_local_rendering_device()
+	
+	# Create shader and pipeline
+	var shader_file = load("res://compute_example.glsl")
+	var shader_spirv = shader_file.get_spirv()
+	var shader = rd.shader_create_from_spirv(shader_spirv)
+	var pipeline = rd.compute_pipeline_create(shader)
+ 	
+	var output_data = _init_output_texture(rd, 2)
+	var output_data_texture = output_data['texture']
+	var output_data_uniform = output_data['uniform']
+	
+	var height_map = _init_heightmap()	
+	var input_data = _init_output_texture(rd)
+	rd.texture_update(input_data['texture'], 0, height_map.get_data())
+	
+	var uniforms := [output_data_uniform, input_data['uniform']]	
+
+ 
+	
+	# Start compute list to start recording our compute commands
+	var compute_list = rd.compute_list_begin()
+	# Bind the pipeline, this tells the GPU what shader to use
+	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
+	# Binds the uniform set with the data we want to give our shader
+	var uniform_set := rd.uniform_set_create(uniforms, shader, 0)
+	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
+	
+	
+		
+	@warning_ignore("integer_division")
+	rd.compute_list_dispatch(compute_list, texture.get_width()/8, texture.get_height()/8, 1)
+	#rd.compute_list_add_barrier(compute_list)
+	# Tell the GPU we are done with this compute task
+	rd.compute_list_end()
+	# Force the GPU to start our commands
+	rd.submit()
+	# Force the CPU to wait for the GPU to finish with the recorded commands
+	rd.sync()
+	
+	var data : PackedByteArray = rd.texture_get_data(output_data_texture, 0)
+	
+	@warning_ignore("integer_division")
+	var img : Image = (Image.create_from_data(texture.get_width()/2,  texture.get_height()/2, false, Image.FORMAT_L8, data))
+	var tex := ImageTexture.create_from_image(img)
+	texture = tex
+	
+	rd.free_rid(output_data_texture) 
+	rd.free_rid(input_data['texture']) 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
