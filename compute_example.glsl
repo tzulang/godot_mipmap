1c5dbefde566490e7e11ecc63bf21a3352d5dbba
yonatz <yonatan.tzulang@gmail.com>
2024-06-28 15:06:54 +0300

    blocks work

--- compute_example.glsl
+++ compute_example.glsl
@@ -4,15 +4,14 @@
 layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
 layout(r8, binding = 0) restrict uniform image2D output_data;
 layout(r8, binding = 1) restrict readonly uniform image2D input_data;
+ 
+
+layout(set = 0, binding = 2, std430) restrict readonly buffer a { int x_sizes[];};
+layout(set = 0, binding = 3, std430) restrict readonly buffer b { int x_offset[];};
+layout(set = 0, binding = 4, std430) restrict readonly buffer c { int y_sizes[];};
+layout(set = 0, binding = 5, std430) restrict readonly buffer d { int y_offset[];};
 
 
-layout(set = 0, binding = 2, std430) restrict readonly buffer a {
-    int x_sizes[];
-} ;
-
-layout(set = 0, binding = 3, std430) restrict readonly buffer b {
-    int y_sizes[];
-};
 
 
 float get_max_value(float a, float b, float c, float d) {
@@ -29,16 +28,39 @@ void main() {
 
  	ivec2 uv_input = uv * 2;
 
+ 	int width = x_sizes[1];
+ 	int heigt = y_sizes[1];
 
- 	float value = get_max_value(
- 			   	  	 imageLoad(input_data , uv_input		 		).x,
- 	  	   			 imageLoad(input_data , uv_input + ivec2(1, 0)	).x,
- 	  	   			 imageLoad(input_data , uv_input + ivec2(1, 1)	).x,
- 	  	   			 imageLoad(input_data , uv_input + ivec2(0, 1)	).x);
+
+ 	float value=0;
+	
+	int n = int(x_sizes[0]);
  	
  	
- 	  
-	vec4 pixel = vec4(vec3(value), 0.0);
+ 	for (int i=1; i < n ; i++){
+ 	 	float color = 1. - 1./float(n) * float(i-1);
+ 	 	float x_min = x_offset[i];
+ 	 	float x_max = x_offset[i] + x_sizes[i];
+ 	 	float y_min = y_offset[i];
+ 	 	float y_max = y_offset[i] + y_sizes[i];
+ 	 	if ((x_min <= uv.x && uv.x < x_max) && (y_min <= uv.y && uv.y < y_max)) {
+ 	 	  	value = color;
+ 	 	}
+
+
+ 	}
+ 	// float value = get_max_value(
+ 	// 		   	  	 imageLoad(input_data, uv_input).x,
+ 	//   	   			 imageLoad(input_data, uv_input + ivec2(1, 0)).x,
+ 	//   	   			 imageLoad(input_data, uv_input + ivec2(1, 1)).x,
+ 	//   	   			 imageLoad(input_data, uv_input + ivec2(0, 1)).x);
+ 	
+
+	 
+	vec4 pixel = vec4(vec3(value), 1.0);
 	imageStore(output_data, uv, pixel);
+
+
+
 
 }
--- first_scene.gd
+++ first_scene.gd
@@ -1,107 +1,135 @@
 extends Sprite2D
- 
+
 var _bind_counter: int = 0;
 
-func _get_buffer_size(s: int)-> PackedInt32Array :
-	
+
+func _get_buffer_size(s: int)-> PackedInt32Array:
+
 	var arr := PackedInt32Array()
+	arr.append(0)
 	while s > 1:
-		 
+
 		arr.append(s)
 		@warning_ignore("integer_division")
-		s = int(s/2) + s%2
+		s = int(s / 2) + (s % 2)
 	arr.append(s)
-	var res := PackedInt32Array([arr.size()])
-	res.append_array(arr)
-	return res 
+	arr[0] = arr.size()
+	return arr
 
-func _array_sum(arr :Array[int])->int:
-	return arr.reduce(func(a,b): return a + b, 0)
 
-func _get_buffer_sizes(x: int,  y: int)	:	
-	
-	var x_size := _get_buffer_size(x)
-	var y_size := _get_buffer_size(y)
-	
-	return [x_size, y_size]
-	
-	
-func _init_stoarge_buffer(rd: RenderingDevice, array: PackedInt32Array):
-	
-	var input_bytes = array.to_byte_array()
-	var buffer := rd.storage_buffer_create(input_bytes.size(), input_bytes)
-	
+func calc_offset_buffer(buff: PackedInt32Array, b: int)->PackedInt32Array:
+
+	var res     := PackedInt32Array(buff)
+	var _offset =  0
+	for i in range(1, len(buff)):
+		res[i] = _offset
+		_offset+= buff[i] * abs    (b-i % 2)
+	res[0] = res.size()
+
+	return res
+
+
+func _array_sum(arr: Array[int])->int:
+	return arr.reduce(func(a, b): return a + b, 0)
+
+
+func _get_size_and_offsets_buffers(x: int, y: int):
+
+	var x_size: = _get_buffer_size(x)
+	var x_offsets := calc_offset_buffer(x_size, 0)
+	var y_size    := _get_buffer_size(y)
+	var y_offsets := calc_offset_buffer(y_size, 1)
+	print(x_size)
+	print(x_offsets)
+	print(y_size)
+	print(y_offsets)
+	return [x_size.to_byte_array(), x_offsets.to_byte_array(),
+	y_size.to_byte_array(), y_offsets.to_byte_array()]
+
+
+func _init_storage_buffer(rd: RenderingDevice, array: PackedByteArray):
+
+	var buffer := rd.storage_buffer_create(array.size(), array)
+
 	var uniform := RDUniform.new()
 	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
-	uniform.binding = _bind_counter 
+	uniform.binding = _bind_counter
 	_bind_counter += 1
-	uniform.add_id(buffer) 
-	
+	uniform.add_id(buffer)
+
 	return {'buffer': buffer, 'uniform': uniform }
 
+
 func _init_heightmap():
-	
+
 	var heightmap := Image.new()
 	heightmap.copy_from( texture.get_image())
 	heightmap.convert(Image.FORMAT_L8)
-	
+
 	return heightmap
-	
+
 
 func _init_output_texture(rd: RenderingDevice, factor = [1.0, 1.0]):
 	var fmt := RDTextureFormat.new()
-	
+
 	@warning_ignore("integer_division")
 	fmt.width = int(ceil(texture.get_width() / factor[0]))
 	fmt.height = int(ceil(texture.get_width() / factor[1]))
-	
+
 	fmt.format = RenderingDevice.DATA_FORMAT_R8_UNORM
 	fmt.usage_bits = RenderingDevice.TEXTURE_USAGE_CAN_UPDATE_BIT | \
-					 RenderingDevice.TEXTURE_USAGE_STORAGE_BIT | \
-					 RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT
+	RenderingDevice.TEXTURE_USAGE_STORAGE_BIT | \
+	RenderingDevice.TEXTURE_USAGE_CAN_COPY_FROM_BIT
 
 	var output_data_texture: RID = rd.texture_create(fmt, RDTextureView.new())
-	
+
 	var output_data_uniform := RDUniform.new()
 	output_data_uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_IMAGE
 	output_data_uniform.binding = _bind_counter
 	output_data_uniform.add_id(output_data_texture)
-	
+
 	_bind_counter+=1
-	
+
 	return {'buffer': output_data_texture, 'uniform': output_data_uniform}
-	
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	await texture.changed
 	# We will be using our own RenderingDevice to handle the compute commands
-	var rd : RenderingDevice = RenderingServer.create_local_rendering_device()
-	
+	var rd: RenderingDevice = RenderingServer.create_local_rendering_device()
+
 	# Create shader and pipeline
-	var shader_file = load("res://compute_example.glsl")
+	var shader_file  = load("res://compute_example.glsl")
 	var shader_spirv = shader_file.get_spirv()
-	var shader = rd.shader_create_from_spirv(shader_spirv)
-	var pipeline = rd.compute_pipeline_create(shader)
-	 	
-	var factor := [4. / 3., 2.] ;
-	
-	var output_data = _init_output_texture(rd, factor)
+	var shader       = rd.shader_create_from_spirv(shader_spirv)
+	var pipeline     = rd.compute_pipeline_create(shader)
+
+	var factor := [1., 1.] ;
+
+	var output_data         = _init_output_texture(rd, factor)
 	var output_data_texture = output_data['buffer']
- 	
-	var height_map = _init_heightmap()	
+
+	var height_map = _init_heightmap()
 	var input_data = _init_output_texture(rd)
 	rd.texture_update(input_data['buffer'], 0, height_map.get_data())
-	
-	var size_data = _get_buffer_sizes(texture.get_width(), texture.get_height())
-	var x_size_buf = _init_stoarge_buffer(rd, size_data[0])
-	var y_size_buf = _init_stoarge_buffer(rd, size_data[1])
-		
-	var uniforms := [output_data['uniform'], 
-					 input_data['uniform'],
-					 x_size_buf['uniform'],
-					 y_size_buf['uniform'],
-					]	
-	
+
+
+	@warning_ignore("integer_division")
+	var size_data  = _get_size_and_offsets_buffers(texture.get_width()/2, texture.get_height()/2)
+	var x_size_buf = _init_storage_buffer(rd, size_data[0])
+	var x_size_off = _init_storage_buffer(rd, size_data[1])
+	var y_size_buf = _init_storage_buffer(rd, size_data[2])
+	var y_size_off = _init_storage_buffer(rd, size_data[3])
+
+	var uniforms := [output_data['uniform'],
+					input_data['uniform'],
+					x_size_buf['uniform'],
+					x_size_off['uniform'],
+					y_size_buf['uniform'],
+					y_size_off['uniform']
+					]
+
 	# Start compute list to start recording our compute commands
 	var compute_list = rd.compute_list_begin()
 	# Bind the pipeline, this tells the GPU what shader to use
@@ -109,11 +137,11 @@ func _ready():
 	# Binds the uniform set with the data we want to give our shader
 	var uniform_set := rd.uniform_set_create(uniforms, shader, 0)
 	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
-	
-	
-		
+
+
+
 	@warning_ignore("integer_division")
-	
+
 	rd.compute_list_dispatch(compute_list, texture.get_width()/8, texture.get_height()/8, 1)
 	#rd.compute_list_add_barrier(compute_list)
 	# Tell the GPU we are done with this compute task
@@ -122,21 +150,23 @@ func _ready():
 	rd.submit()
 	# Force the CPU to wait for the GPU to finish with the recorded commands
 	rd.sync()
-	
-	var data : PackedByteArray = rd.texture_get_data(output_data_texture, 0)
-	
+
+	var data: PackedByteArray = rd.texture_get_data(output_data_texture, 0)
+
 	@warning_ignore("integer_division")
-	
-	var img : Image = (Image.create_from_data(int(ceil(texture.get_width()/factor[0])),  
-											  int(ceil(texture.get_height()/factor[1])), 
-											  false, Image.FORMAT_L8, data))
-	var tex := ImageTexture.create_from_image(img)
+
+	var img: Image =  (Image.create_from_data(int(ceil(texture.get_width()/factor[0])),
+	int(ceil(texture.get_height()/factor[1])),
+	false, Image.FORMAT_L8, data))
+	var tex        := ImageTexture.create_from_image(img)
 	texture = tex
-	
-	rd.free_rid(output_data['buffer']) 
-	rd.free_rid(input_data['buffer']) 
-	rd.free_rid(x_size_buf['buffer']) 
-	rd.free_rid(y_size_buf['buffer']) 
+
+	rd.free_rid(output_data['buffer'])
+	rd.free_rid(input_data['buffer'])
+	rd.free_rid(x_size_buf['buffer'])
+	rd.free_rid(x_size_off['buffer'])
+	rd.free_rid(y_size_buf['buffer'])
+	rd.free_rid(y_size_off['buffer'])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
